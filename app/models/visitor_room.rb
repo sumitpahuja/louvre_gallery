44d6bad009995acf8fe9cdb61fb7f35304a96fb0
sumitpahuja <sumit.pahuja1@gmail.com>
2018-03-15 17:20:59 +0530

    change db adapter

--- app/models/visitor_room.rb
+++ app/models/visitor_room.rb
@@ -1,23 +1,19 @@
 class VisitorRoom
-  extend ActiveModel::Naming
-  Struct.new(:room_id, :room_id, :in_out, :minute)
+  # extend ActiveModel::Naming
+  # Struct.new(:room_id, :room_id, :in_out, :minute)
 
 
   
-  # attr_reader :char
+  # # attr_reader :char
 
-  def initialize(:visitor_id, :room_id, :in_out, :minute)
-    @visitor_id, @room_id, @in_out, @minute = :visitor_id, :room_id, :in_out, :minute
-  end
-
-
-
-
-  def to_model
-    # You will get to_model error, if you don't have this dummy method
-  end
-  # You need this otherwise you get an error
-  def persisted?
-    false
-  end
+  # # def initialize(:visitor_id, :room_id, :in_out, :minute)
+  # #   @visitor_id, @room_id, @in_out, @minute = :visitor_id, :room_id, :in_out, :minute
+  # # end
+  # def to_model
+  #   # You will get to_model error, if you don't have this dummy method
+  # end
+  # # You need this otherwise you get an error
+  # def persisted?
+  #   false
+  # end
 end
